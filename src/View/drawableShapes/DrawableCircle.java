9ff8bb337987b4a6028fb9b4615d93c775c1f262
jpetmecky23 <jpetmecky23@gmail.com>
2015-01-20 12:24:01 -0700

    DrawableClasses

---
+++ src/View/drawableShapes/DrawableCircle.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.drawableShapes;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author James
+ */
+public class DrawableCircle extends DrawableShape{
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public DrawableCircle(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public void draw(Graphics2D g2d){
+        g2d.setColor(color);
+        g2d.fillOval(x, y, width, height);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    
+}
---
+++ src/View/drawableShapes/DrawableTri.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.drawableShapes;
+
+import Model.shapes.Point3D;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+
+/**
+ *
+ * @author James
+ */
+public class DrawableTri extends DrawableShape{
+    private Point3D p1;
+    private Point3D p2;
+    private Point3D p3;
+    
+    public DrawableTri(Point3D p1, Point3D p2, Point3D p3, Color color) {
+        this.color = color;
+    }
+
+    
+    @Override
+    public void draw(Graphics2D g2d){
+        g2d.setColor(color);
+        Polygon p = getPolygon();
+        g2d.fillPolygon(p);
+    }
+    
+    public Polygon getPolygon() {
+        int[] xpoints;
+        int[] ypoints;
+        int npoints = 3; //triangle
+        
+        xpoints = new int[3];
+        xpoints[0] = (int) p1.x;
+        xpoints[1] = (int) p2.x;
+        xpoints[2] = (int) p3.x;
+        
+        ypoints = new int[3];
+        ypoints[0] = (int) p1.y;
+        ypoints[1] = (int) p2.y;
+        ypoints[2] = (int) p3.y;
+        
+        npoints = 3;//three coners on a triangle
+        return new Polygon(xpoints, ypoints, npoints);
+    }
+
+    public Point3D getP1() {
+        return p1;
+    }
+
+    public void setP1(Point3D p1) {
+        this.p1 = p1;
+    }
+
+    public Point3D getP2() {
+        return p2;
+    }
+
+    public void setP2(Point3D p2) {
+        this.p2 = p2;
+    }
+
+    public Point3D getP3() {
+        return p3;
+    }
+
+    public void setP3(Point3D p3) {
+        this.p3 = p3;
+    }
+}
