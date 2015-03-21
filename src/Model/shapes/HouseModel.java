fcf554efba9376381bb591220ca2a25577c97543
jpetmecky23 <jpetmecky23@gmail.com>
2015-03-21 10:36:03 -0600

    Fixed House Model
    Added color to the house model

--- src/Model/shapes/HouseModel.java
+++ src/Model/shapes/HouseModel.java
@@ -2,8 +2,8 @@ package Model.shapes;
 
 
 
-import Model.shapes.Point3D;
-import Model.shapes.Line3D;
+import Model.shapes.*;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,34 +16,34 @@ public class HouseModel extends WireFrame
 {
     List<Line3D> lines = new ArrayList<>();
     
-    public HouseModel()
+    public HouseModel(Color color)
     {
         //Make the object:
          //Floor
-       /* lines.add(new Line3D(new Point3D(-5,0,-5), new Point3D(5,0,-5)));
-        lines.add(new Line3D(new Point3D(5,0,-5), new Point3D(5,0,5)));
-        lines.add(new Line3D(new Point3D(5,0,5), new Point3D(-5,0,5)));
-        lines.add(new Line3D(new Point3D(-5,0,5), new Point3D(-5,0,-5)));
+        lines.add(new Line3D(new Point3D(-5,0,-5), new Point3D(5,0,-5), color));
+        lines.add(new Line3D(new Point3D(5,0,-5), new Point3D(5,0,5), color));
+        lines.add(new Line3D(new Point3D(5,0,5), new Point3D(-5,0,5), color));
+        lines.add(new Line3D(new Point3D(-5,0,5), new Point3D(-5,0,-5), color));
          //Ceiling
-        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(5,5,-5)));
-        lines.add(new Line3D(new Point3D(5,5,-5), new Point3D(5,5,5)));
-        lines.add(new Line3D(new Point3D(5,5,5), new Point3D(-5,5,5)));
-        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(-5,5,-5)));
+        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(5,5,-5), color));
+        lines.add(new Line3D(new Point3D(5,5,-5), new Point3D(5,5,5), color));
+        lines.add(new Line3D(new Point3D(5,5,5), new Point3D(-5,5,5), color));
+        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(-5,5,-5), color));
          //Walls
-        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(-5,0,-5)));
-        lines.add(new Line3D(new Point3D(5,5,-5), new Point3D(5,0,-5)));
-        lines.add(new Line3D(new Point3D(5,5,5), new Point3D(5,0,5)));
-        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(-5,0,5)));        
+        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(-5,0,-5), color));
+        lines.add(new Line3D(new Point3D(5,5,-5), new Point3D(5,0,-5), color));
+        lines.add(new Line3D(new Point3D(5,5,5), new Point3D(5,0,5), color));
+        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(-5,0,5), color));        
          //Roof
-        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(0,8,-5)));
-        lines.add(new Line3D(new Point3D(0,8,-5), new Point3D(5,5,-5)));
-        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(0,8,5)));
-        lines.add(new Line3D(new Point3D(0,8,5), new Point3D(5,5,5)));
-        lines.add(new Line3D(new Point3D(0,8,-5), new Point3D(0,8,5)));
+        lines.add(new Line3D(new Point3D(-5,5,-5), new Point3D(0,8,-5), color));
+        lines.add(new Line3D(new Point3D(0,8,-5), new Point3D(5,5,-5), color));
+        lines.add(new Line3D(new Point3D(-5,5,5), new Point3D(0,8,5), color));
+        lines.add(new Line3D(new Point3D(0,8,5), new Point3D(5,5,5), color));
+        lines.add(new Line3D(new Point3D(0,8,-5), new Point3D(0,8,5), color));
          //Door
-        lines.add(new Line3D(new Point3D(1,0,5), new Point3D(1,3,5)));
-        lines.add(new Line3D(new Point3D(-1,0,5), new Point3D(-1,3,5)));
-        lines.add(new Line3D(new Point3D(1,3,5), new Point3D(-1,3,5)));*/
+        lines.add(new Line3D(new Point3D(1,0,5), new Point3D(1,3,5), color));
+        lines.add(new Line3D(new Point3D(-1,0,5), new Point3D(-1,3,5), color));
+        lines.add(new Line3D(new Point3D(1,3,5), new Point3D(-1,3,5), color));
     }
     
     
